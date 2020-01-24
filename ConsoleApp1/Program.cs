34768445b6a40a6b35d53ace33e09350baf570c0
Anna Friedman <jtfriedman09@gmail.com>
2020-01-24 08:45:17 -0500

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Pause to display Hello World!
+            Console.WriteLine("Press any key to continue...");
+
+            Console.WriteLine("Hello World!");
+            Console.ReadKey(true);
+        }
+    }
+}
